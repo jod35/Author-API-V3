edebac5029b9030e7e7cd4936a31a85d4d845543
jod35 <jodestrevin@gmail.com>
2020-07-28 15:55:44 -0700

    create an author resource

--- main/views.py
+++ main/views.py
@@ -2,6 +2,10 @@ from main.models import Author,AuthorSchema
 from flask import request,jsonify,make_response
 from main import app,db
 
+##########################################
+######### Get all authors ################
+##########################################
+
 @app.route('/authors',methods=['GET'])
 def get_all_authors():
     all_authors=Author.objects.all()
@@ -17,5 +21,31 @@ def get_all_authors():
         )
     )
 
+
+#######################################
+######## Create an author #############
+#######################################
+
+@app.route('/authors',methods=['POST'])
+def create_new_author():
+    
+    data=request.get_json()
+
+    new_author=Author(name=data['name'],specialization=data['specialization'])
+
+    new_author.save()
+
+    author_schema=AuthorSchema(only=['name','specialization'])
+
+    author=author_schema.dump(new_author)
+
+    return make_response(
+        jsonify(
+            {"message":"Author Created Successfully",
+            "author":author}
+        ),201
+    )
+
+    author=author_schema.dump(new_author)
 
 
